68ae6563efc13d089f5d5a2c21844a14a9db913a
Dotneteer <dotneteer@hotmail.com>
2020-02-27 07:00:02 +0100

    Unused files removed.

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import * as logger from "morgan";
 import * as path from "path";
 
 var indexRouter = require('../src/routes/index');
-var testRouter = require('../src/routes/tests');
 
 var app = express();
 
@@ -19,7 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../src/public')));
 app.use('/', indexRouter);
-app.use('/tests', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
